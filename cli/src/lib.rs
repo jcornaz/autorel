d6bb36fc66eb309174916824d483c5baa24d7f0d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2021-04-10 20:16:28 +0200

    feat: Command line interface

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,3 +1,14 @@
 #![deny(future_incompatible)]
 #![warn(nonstandard_style, rust_2018_idioms)]
 #![warn(clippy::pedantic)]
+
+#[macro_use]
+extern crate clap;
+
+use clap::AppSettings;
+pub use clap::Clap;
+
+#[derive(Clap)]
+#[clap(name = "autorel", version = crate_version ! (), author = crate_authors ! (), about = "Software release automation")]
+#[clap(setting = AppSettings::ColoredHelp)]
+pub struct Command;
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,8 @@
 #![warn(nonstandard_style, rust_2018_idioms)]
 #![warn(clippy::pedantic)]
 
-fn main() {}
+use autorel_cli::{Clap, Command};
+
+fn main() {
+    let _: Command = Command::parse();
+}
