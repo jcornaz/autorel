42d9dd1edbf43c7525e07f08f7df75bef45d060d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2021-05-13 15:14:25 +0200

    refactor: Move parsing logic into a parser module

---
+++ core/src/conventional_commit_parser.rs
@@ -0,0 +1,50 @@
+use pest::Parser;
+
+use super::*;
+
+#[derive(Parser)]
+#[grammar = "conventional_commit.pest"]
+struct ConventionalCommitParser;
+
+pub(crate) fn parse(commit_msg: &str) -> Option<Change> {
+    let commit = ConventionalCommitParser::parse(Rule::conventional_commit, commit_msg)
+        .ok()?
+        .next()?;
+
+    let mut result = Change {
+        type_: ChangeType::Fix,
+        breaking: BreakingInfo::NotBreaking,
+        scope: None,
+        description: "",
+        body: None,
+    };
+
+    for commit_part in commit.into_inner() {
+        match commit_part.as_rule() {
+            Rule::feat => result.type_ = ChangeType::Feature,
+            Rule::fix => result.type_ = ChangeType::Fix,
+            Rule::custom_type => result.type_ = ChangeType::Custom(commit_part.as_str()),
+            Rule::breaking_flag => result.breaking = BreakingInfo::Breaking,
+            Rule::scope => result.scope = Some(commit_part.as_str()),
+            Rule::description => result.description = commit_part.as_str(),
+            Rule::body => result.body = Some(commit_part.as_str()),
+            Rule::footer => {
+                let mut is_breaking = false;
+                let mut footer_content: &str = "";
+                for footer_part in commit_part.into_inner() {
+                    match footer_part.as_rule() {
+                        Rule::breaking_change_token => is_breaking = true,
+                        Rule::footer_value => footer_content = footer_part.as_str(),
+                        _ => (),
+                    }
+                }
+                if is_breaking {
+                    result.breaking = BreakingInfo::BreakingWithDescription(footer_content);
+                }
+            }
+            _ => (),
+        }
+    }
+
+    Some(result)
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,7 +2,8 @@
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
+mod changelog;
+mod conventional_commit_parser;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Change<'a> {
@@ -34,11 +35,11 @@ pub enum SemverScope {
     Breaking,
 }
 
-#[derive(Parser)]
-#[grammar = "conventional_commit.pest"]
-struct ConventionalCommitParser;
-
 impl<'a> Change<'a> {
+    pub fn parse_conventional_commit(message: &'a str) -> Option<Self> {
+        conventional_commit_parser::parse(message)
+    }
+
     pub fn semver_scope(&self) -> Option<SemverScope> {
         match (self.breaking, self.type_) {
             (BreakingInfo::NotBreaking, ChangeType::Feature) => Some(SemverScope::Feature),
@@ -48,49 +49,5 @@ impl<'a> Change<'a> {
                 Some(SemverScope::Breaking)
             }
         }
-    }
-
-    pub fn parse_conventional_commit(message: &'a str) -> Option<Self> {
-        let commit = ConventionalCommitParser::parse(Rule::conventional_commit, message)
-            .ok()?
-            .next()
-            .unwrap();
-
-        let mut result = Self {
-            type_: ChangeType::Fix,
-            breaking: BreakingInfo::NotBreaking,
-            scope: None,
-            description: "",
-            body: None,
-        };
-
-        for commit_part in commit.into_inner() {
-            match commit_part.as_rule() {
-                Rule::feat => result.type_ = ChangeType::Feature,
-                Rule::fix => result.type_ = ChangeType::Fix,
-                Rule::custom_type => result.type_ = ChangeType::Custom(commit_part.as_str()),
-                Rule::breaking_flag => result.breaking = BreakingInfo::Breaking,
-                Rule::scope => result.scope = Some(commit_part.as_str()),
-                Rule::description => result.description = commit_part.as_str(),
-                Rule::body => result.body = Some(commit_part.as_str()),
-                Rule::footer => {
-                    let mut is_breaking = false;
-                    let mut footer_content: &str = "";
-                    for footer_part in commit_part.into_inner() {
-                        match footer_part.as_rule() {
-                            Rule::breaking_change_token => is_breaking = true,
-                            Rule::footer_value => footer_content = footer_part.as_str(),
-                            _ => (),
-                        }
-                    }
-                    if is_breaking {
-                        result.breaking = BreakingInfo::BreakingWithDescription(footer_content);
-                    }
-                }
-                _ => (),
-            }
-        }
-
-        Some(result)
     }
 }
